b33b99ddcae3316a6bf52d3d6fd12af4257e5111
Luiz Vidal <luizfernandovidalsilva1@gmail.com>
2022-12-10 19:22:04 -0300

    :construction: typing challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'typing-challenge';
+export class AppComponent  {
+  randomText = faker.lorem.sentence(10);
+  enteredText = '';
+
+  onInput(event: any) {
+    this.enteredText = event.value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string): string {
+    if (!enteredLetter) return 'pending';
+    return (randomLetter === enteredLetter) ? 'correct' : 'incorrect';
+  }
+
 }
